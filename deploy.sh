aaf3b238d4e58cfe24059eb0616bb110914bf967
Michael Lee <MICHAEL.LEE@INFINITYWORKS.com>
2017-05-16 11:29:44 +0100

    Deploy script debug

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
-./de-secret.sh && ./gradlew uploadArchives closeAndReleaseRepository
+echo "Running in..."
+pwd
+echo "dir listing:"
+ls
+echo "Decrypting magic"
+./de-secret.sh
+echo "Decrypted"
+ls ~/.gradle/
+ls ~/.gnupg/
+echo "Gradling the gradles"
+./gradlew uploadArchives closeAndReleaseRepository
+echo "Sorted!"
